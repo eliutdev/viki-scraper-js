5d0b48a2f781ee7346ff14ab6412f2724e943009
R. Eliut <63687573+eliutdev@users.noreply.github.com>
2022-04-26 01:55:58 +0200

    aa

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,13 @@ type Options = {
 };
 
 const getEpisodeId = (url: string) => {
-  return url.match(/tv\/(\w+)/)[1];
+  const result = url.match(/tv\/(\w+)/);
+  if (result?.length) {
+    return result[1];
+  }
 };
 
-const fetchSynopsis = async ({ url, language }: Options) => {
+const fetchSynopsis = async ({ url, language = "en" }: Options) => {
   const id = getEpisodeId(url);
   const response = await fetch(
     `https://api.viki.io/v4/containers/${id}/descriptions/${language}.json?html=true&app=100000a`
@@ -41,37 +44,42 @@ export const getInfo = async ({ url, language = "en" }: Options) => {
 
   const title = document.querySelector("h1");
   const originalTitle =
-    document.querySelector("h1").parentElement.nextElementSibling;
+    document.querySelector("h1")!.parentElement!.nextElementSibling;
 
   const image = document
-    .querySelector("main img")
-    .getAttribute("src")
-    .split("?")[0]!;
+    .querySelector("main img")!
+    .getAttribute("src")!
+    .split("?")[0];
 
   const [year, rating, episodes] = Array.from(
-    document.querySelector("h1").parentElement.nextElementSibling
-      .nextElementSibling.children
+    document.querySelector("h1")!.parentElement!.nextElementSibling!
+      .nextElementSibling!.children
   ).map((el) => el.textContent);
 
   const [genres, cast] = Array.from(
-    document.querySelector("h1").parentElement.parentElement.nextElementSibling
-      .children
+    document.querySelector("h1")!.parentElement!.parentElement!
+      .nextElementSibling!.children
   )
     .map((el) => {
       const childs = Array.from(el.querySelectorAll("span"));
-      return childs.map((el) => el.textContent.split(","));
+      if (!childs.length) return [];
+      return childs.map((el) => {
+        if (el && el.textContent) {
+          return el.textContent.split(",");
+        }
+      });
     })
     .flat();
 
   const synopsis = await fetchSynopsis({ url, language });
 
   return {
-    title: title.textContent,
-    originalTitle: originalTitle.textContent,
+    title: title ? title.textContent : "",
+    originalTitle: originalTitle ? originalTitle.textContent : "",
     image,
-    year: year.replace(/\D/g, ""),
-    rating: rating.replace(/\D/g, ""),
-    episodes: episodes.replace(/\D/g, ""),
+    year: year && year.replace(/\D/g, ""),
+    rating: rating && rating.replace(/\D/g, ""),
+    episodes: episodes && episodes.replace(/\D/g, ""),
     genres,
     cast,
     synopsis,
